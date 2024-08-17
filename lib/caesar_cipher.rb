3c5c92130b6bf56f8fca62e690b638ac5fb4511c
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-08-17 02:45:31 -0400

    Completed RSpec test for CaesarCipher class
    Additonally, further refactored code to improve
    ability to test.
    
    This commit includes the refactored
    lib/caesar_cipher.rb.
    
    Commits for the new encryptor class and imput
    class will come with separate classes.
    
    Further encapsulation allows us to focus on
    functionality of methods, and reserve testing of
    other methods concurrently with instance doubles.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -4,25 +4,24 @@ require 'input'
 require 'encryptor'
 # app to encrypt & decrypt strings
 class CaesarCipher
-  attr_reader :encryptor
-
-  include Input
+  attr_reader :encryptor, :input
 
   # Function is essentially an array of letters in order
-  def initialize
-    @encryptor = Encryptor.new
+  def initialize(encryptor = Encryptor.new, input = Input.new)
+    @encryptor = encryptor
+    @input = input
   end
 
   def run
-    choice = input('Do you want to encrypt or decrypt a message? ([e]ncrypt/[d]ecrypt)')
+    choice = input.get('Do you want to encrypt or decrypt a message? ([e]ncrypt/[d]ecrypt)')
+    until %w[d e decrypt encrypt].include?(choice)
+      choice = input.get("Input Error! Please choose 'encrypt'/'decrypt' (or just 'e' / 'd')")
+    end
     choose(choice)
   end
 
   def choose(choice)
-    until %w[d e].include?(choice.chars.first)
-      choice = input("Input Error! Please choose 'encrypt'/'decrypt' (or just 'e' / 'd')")
-    end
-    encryptor.encrypt if choice.chars.first == 'e'
-    encryptor.decrypt if choice.chars.first == 'd'
+    encryptor.encrypt if %w[e encrypt].include?(choice)
+    encryptor.decrypt if %w[d decrypt].include?(choice)
   end
 end
